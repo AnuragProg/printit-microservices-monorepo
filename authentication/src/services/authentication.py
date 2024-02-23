efc5ffbd190faf049af1408eca559484a8aaead9
Anurag Singh <anuragcollege7@gmail.com>
2024-02-23 13:56:19 +0545

    updated protobuf definition to use user type as an enum rather than a string

--- authentication/src/services/authentication.py
+++ authentication/src/services/authentication.py
@@ -1,9 +1,9 @@
 import grpc
 from proto_gen.auth_pb2_grpc import AuthenticationServicer
-from proto_gen.auth_pb2 import Token, User
+from proto_gen.auth_pb2 import Token, User, UserType as GrpcUserType
 from grpc.aio import ServicerContext
 from util.jwt_generator import decode_jwt
-from model.user import UserModel
+from model.user import UserModel, UserType as LocalUserType
 
 
 class AuthenticationService(AuthenticationServicer):
@@ -14,11 +14,16 @@ class AuthenticationService(AuthenticationServicer):
         try:
             id = decode_jwt(token=request.token)['_id']
             user = await self.user_model.get_user_from_db_by_id(id)
+            user_type = GrpcUserType.UNDEFINED
+            if user.user_type == LocalUserType.CUSTOMER.value:
+                user_type = GrpcUserType.CUSTOMER
+            elif user.user_type == LocalUserType.SHOPKEEPER.value:
+                user_type = GrpcUserType.SHOPKEEPER
             return User(
                 _id=id,
                 name=user.name,
                 email=user.email,
-                user_type=user.user_type
+                user_type=user_type
             )
         except Exception:
             context.set_code(grpc.StatusCode.UNAUTHENTICATED)
