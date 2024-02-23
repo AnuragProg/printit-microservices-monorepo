9efec18141f036bd7b7652d1d3d84dbc6fb92be5
Anurag Singh <anuragcollege7@gmail.com>
2024-02-23 14:00:20 +0545

    added shop service folder

--- shop/target/debug/deps/shop-32f2998f02089e8d.d
+++
@@ -1,9 +0,0 @@
-F:\Serious Projects\PrintIt Startup\Source Code\printit-microservices-monorepo\shop\target\debug\deps\libshop-32f2998f02089e8d.rmeta: src\main.rs Cargo.toml
-
-F:\Serious Projects\PrintIt Startup\Source Code\printit-microservices-monorepo\shop\target\debug\deps\shop-32f2998f02089e8d.d: src\main.rs Cargo.toml
-
-src\main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- shop/target/debug/deps/shop-9ee9b150df4c6795.d
+++
@@ -1,9 +0,0 @@
-F:\Serious Projects\PrintIt Startup\Source Code\printit-microservices-monorepo\shop\target\debug\deps\libshop-9ee9b150df4c6795.rmeta: src\main.rs Cargo.toml
-
-F:\Serious Projects\PrintIt Startup\Source Code\printit-microservices-monorepo\shop\target\debug\deps\shop-9ee9b150df4c6795.d: src\main.rs Cargo.toml
-
-src\main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
