160c17404159c657fd8affe0efa223f9d3a671c1
Anurag Singh <anuragcollege7@gmail.com>
2024-02-25 17:28:05 +0545

    added initial code for file service

--- file/cmd/main.go
+++ file/cmd/main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
 	"github.com/gofiber/fiber/v2"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	route "github.com/AnuragProg/printit-microservices-monorepo/file/internal/api/routes"
+	auth "github.com/AnuragProg/printit-microservices-monorepo/file/proto_gen/authentication"
 )
 
 
 func main(){
+
+	// Setup rest app
 	app := fiber.New()
 
+	// connect to mongo database
+	mongo_ctx, mongo_ctx_cancel := context.WithCancel(context.Background())
+	defer mongo_ctx_cancel()
+	mongo_client, err := mongo.Connect(mongo_ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil{
+		panic(err.Error())
+	}
+	log.Println("Connected to Mongo...")
+
+	// connect to grpc servers
+	auth_grpc_conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil{
+		panic(err.Error())
+	}
+	defer auth_grpc_conn.Close()
+	log.Println("Connected to Auth GRPC server...")
+
+	// create grpc clients
+	auth_grpc_client := auth.NewAuthenticationClient(auth_grpc_conn)
+
+
+	// setup top level routes
 	fileRouter := app.Group("/file")
 	fileRoute := route.FileRoute{
+		MongoClient: mongo_client,
 		Router: &fileRouter,
-		AuthGrpcClient: nil,
+		AuthGrpcClient: &auth_grpc_client,
 	}
 	fileRoute.SetupRoutes()
 
 
-	app.Listen(":3000")
+	// start rest server
+	log.Println("Listening on 3001")
+	app.Listen(":3001")
 }
--- file/internal/api/routes/file_routes.go
+++ file/internal/api/routes/file_routes.go
@@ -2,13 +2,15 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	handler "github.com/AnuragProg/printit-microservices-monorepo/file/internal/api/handlers"
-	auth "github.com/AnuragProg/printit-microservices-monorepo/file/internal/proto_gen/authentication"
+	auth "github.com/AnuragProg/printit-microservices-monorepo/file/proto_gen/authentication"
 )
 
 
 type FileRoute struct{
+	MongoClient *mongo.Client
 	Router *fiber.Router
 	AuthGrpcClient *auth.AuthenticationClient
 }
